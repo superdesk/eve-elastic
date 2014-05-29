de8465bd6188582b2f5b8b0b40373421a2e12d71
petr.jasek <petr.jasek@sourcefabric.org>
2014-05-29 16:04:34 +0200

    release 0.1.9

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('LICENSE') as f:
 
 setup(
     name='Eve-Elastic',
-    version='0.1.8',
+    version='0.1.9',
     description='Elasticsearch data layer for eve rest framework',
     long_description=readme + '\n\n' + changelog,
     license=license,
