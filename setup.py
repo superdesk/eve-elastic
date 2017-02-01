450f2fdda88784427a676e0febab3186f710d556
Petr Jasek <jasekpetr@gmail.com>
2017-02-01 08:38:05 +0100

    release 2.0rc3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('LICENSE') as f:
 
 setup(
     name='Eve-Elastic',
-    version='2.0rc2',
+    version='2.0rc3',
     description='Elasticsearch data layer for eve rest framework',
     long_description=readme + '\n\n' + changelog,
     license=license,
