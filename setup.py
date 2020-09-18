301a1e4cd3e8b7ae62a9837b1114a97f47e598f8
Petr <petr.jasek@sourcefabric.org>
2020-09-18 11:13:32 +0200

    release 7.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("CHANGELOG.rst") as f:
 
 setup(
     name="Eve-Elastic",
-    version="7.1.0",
+    version="7.1.1",
     description="Elasticsearch data layer for eve rest framework",
     long_description=readme + "\n\n" + changelog,
     license="GPLv3+",
