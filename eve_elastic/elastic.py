d4a2d0e09f45d1c076b1133433071e2d9a327310
Mark Pittaway <mark.pittaway@gmail.com>
2022-11-30 15:26:55 +1100

    Fix passing string value for `query` field

--- eve_elastic/elastic.py
+++ eve_elastic/elastic.py
@@ -216,7 +216,7 @@ def fix_query(query, top=True, context=None):
         elif key == "query_string":
             new_query[key] = val
             val.setdefault("lenient", True)
-        elif key == "query" and not top and context != "aggs":
+        elif key == "query" and not top and context != "aggs" and not isinstance(val, str):
             new_query["bool"] = {"must": fix_query(val, top=False, context=context)}
         elif top:
             new_query[key] = fix_query(val, top=False, context=key)
