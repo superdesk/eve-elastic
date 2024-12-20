b1d7bba16acd6e21d9de0a70ce46fe8584a86b6a
Petr Jašek <jasekpetr@gmail.com>
2024-12-20 07:35:16 +0100

    setup package publishing via github actions (#116)

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 rm -rf dist build
-python -m pip build
-twine check dist/* && twine upload dist/*
+python -m build .
+twine check dist/* && twine upload --repository eve-elastic dist/*
